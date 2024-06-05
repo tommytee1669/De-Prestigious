8cfa5a12f4ee99d42f51a51aa5c734ada6bdd3a3
tommytee1669 <140799987+tommytee1669@users.noreply.github.com>
2024-06-05 15:18:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>
+{hamburger.classList.remove("active");
+ navMenu.classList.length("active");
+}));
+
+document.onclick = function(e){
+    if(!hamburger.contains(e.target)&&!navMenu.contains(e.target)){
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+}
+
+
+
+
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio'+counter).checked = true;
+    counter++;
+
+    if(counter>4){
+        counter = 1;
+    }
+},4000)
+
+
+
+
+
+
+
+
